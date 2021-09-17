44a09a17c1abee46fd9d653eb4ab613fdcea549b
deliretik <deliretik@gmail.com>
2021-09-17 20:36:33 +0200

    first comment

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/* let myName = 'bob';
+document.write(myName);
+myName = 'john doe';
+document.write(myName);
+
+let simpleAddition = 2+2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize *2) - (size / 2);
+document.write(minSize); */
+
+alert('Hello world');
+
+let myfavoriteFood = tagine;
+document.write(myfavoriteFood);/* this is a comment */
+
