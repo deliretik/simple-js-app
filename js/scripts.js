2a1d5adc1eff0173eb4dd20e4f6d0e9727d408e4
deliretik <deliretik@gmail.com>
2021-09-24 13:15:39 +0200

    for loop writing the height + testing different iteration of the for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,49 @@
-pokemonsList= [
-    {name:'Squirtle', type:'water', height:0.5},
-    {name:'Wartortle', type:'water', height:1},
-    {name:'Bulbasaur', type:['grass','poison'], height:0.7}
-]
+// pokemonsList= [
+//     {name:'Squirtle', type:'water', height:0.5},
+//     {name:'Wartortle', type:'water', height:1},
+//     {name:'Bulbasaur', type:['grass','poison'], height:0.7}
+// ]
 
-/*if(pokemons.size > 1.5){console.log('this is a big pokemon!!!')}
-else if(pokemons.size > 0.5 && pokemons.size < 1.5){console.log('this is an average pokemon !!!')}
-else {console.log('this is a small pokemon !!!')}*/
+// /*if(pokemons.size > 1.5){console.log('this is a big pokemon!!!')}
+// else if(pokemons.size > 0.5 && pokemons.size < 1.5){console.log('this is an average pokemon !!!')}
+// else {console.log('this is a small pokemon !!!')}*/
 
-for (let i = 0; pokemonsList.length; i++)
-    if(pokemonsList[i].height <0.7){
-    document.write('<ul><li>' + pokemonsList[i].name + ' is a small pokemon.' + '</li></ul>')}
-    else if(pokemonsList[i].height >=0.7 && pokemonsList[i].height <1){
-    document.write('<ul><li>' + pokemonsList[i].name + ' is an average pokemon.' + '</li></ul>')}
-    else if(pokemonsList[i].height >=1){
-    document.write('<ul><li>' + pokemonsList[i].name + ' is a big pokemon.' + '</li></ul>')};
+// for (let i = 0; pokemonsList.length; i++)
+//     if(pokemonsList[i].height <0.7){
+//     document.write('<ul><li>' + pokemonsList[i].name + ' is a small pokemon.' + '</li></ul>')}
+//     else if(pokemonsList[i].height >=0.7 && pokemonsList[i].height <1){
+//     document.write('<ul><li>' + pokemonsList[i].name + ' is an average pokemon.' + '</li></ul>')}
+//     else if(pokemonsList[i].height >=1){
+//     document.write('<ul><li>' + pokemonsList[i].name + ' is a big pokemon.' + '</li></ul>')};
 
 
+    pokemonsList= [
+      {name:'Squirtle', type:'water', height:0.5},
+      {name:'Wartortle', type:'water', height:1},
+      {name:'Bulbasaur', type:['grass','poison'], height:0.7}
+  ]
+  
+  /*if(pokemons.size > 1.5){console.log('this is a big pokemon!!!')}
+  else if(pokemons.size > 0.5 && pokemons.size < 1.5){console.log('this is an average pokemon !!!')}
+  else {console.log('this is a small pokemon !!!')}*/
+  
+  for (let i = 0; i< pokemonsList.length; i++)
+      if (pokemonsList[i].height <0.7){
+      document.write('<ul><li>' + pokemonsList[i].name + ' is a small pokemon.' + '</li></ul>')}
+      else if (pokemonsList[i].height >=0.7 && pokemonsList[i].height <1){
+      document.write('<ul><li>' + pokemonsList[i].name + ' is an average pokemon.' + '</li></ul>')}
+      else if(pokemonsList[i].height >=1){
+      document.write('<ul><li>' + pokemonsList[i].name + ' is a big pokemon.' + '</li></ul>')};
+  
+  
+  //loop
+   for (let i = 0; i< pokemonsList.length; i++) 
+       if (pokemonsList[i].height <0.7){
+           document.write('<ul><li>' + pokemonsList[i].name + ' ( Height: ' + pokemonsList[i].height + ' )' + ' that\'s a small pokemon.' + '</li></ul>')
+       }else if (pokemonsList[i].height >=0.7 && pokemonsList[i].height <1) {
+           document.write('<ul><li>' + pokemonsList[i].name + ' ( Height: ' + pokemonsList[i].height + ' )' + ' that\'s an average pokemon.' + '</li></ul>')
+      //  }else if(pokemonsList[i].height >=1){
+       }else{
+           document.write('<ul><li>' + pokemonsList[i].name + ' ( Height: ' + pokemonsList[i].height + ' )' + ' that\'s a big pokemon.' + '</li></ul>')};
+       
+ 
