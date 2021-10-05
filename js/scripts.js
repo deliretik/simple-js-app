6155ccde9bd2d9fe257b2c4c011459a3c509ffd4
deliretik <deliretik@gmail.com>
2021-10-05 12:20:21 +0200

    updated code with the addEventListener

--- js/scripts.js
+++ js/scripts.js
@@ -19,13 +19,15 @@
         let pokemonButton = document.createElement('button');
           pokemonButton.innerText = pokemon.name;
           pokemonButton.classList.add('button-class');
-          pokemonButton.addEventListener('click', showDetails());
           pokemonListItem.appendChild(pokemonButton);
           pokemonUnorderedList.appendChild(pokemonListItem);
+          pokemonButton.addEventListener('click', function () {
+            showDetails(pokemon)
+          });
       }; 
 
       function showDetails(pokemon){
-        console.log(repository);
+        console.log(pokemon);
       };
       
       return {
@@ -40,5 +42,4 @@
       pokemonRepository.addListItem(pokemon);
     });
     
-    
-    
+  
